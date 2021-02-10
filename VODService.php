59bc0dcf24a27316803cc9bfcc09ccf098618458
Sylvie-B <unsolo@hotmail.fr>
2021-02-10 16:27:25 +0100

    create class

---
+++ VODService.php
@@ -0,0 +1,45 @@
+<?php
+
+
+class VODService
+{
+    private string $name;
+    private float $price;
+
+
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): VODService
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price): VODService
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+}
